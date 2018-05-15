56f0b366955948ba5e80f8a8925e8ba77c5681d9
montgomeryrd <montgomery_rd@yahoo.com>
2018-05-15 01:49:22 -0700

    04-code-bitmap

---
+++ index.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const fs = require('fs');
+const Bmp = require('./lib/bitmap');
+const reader = require('./lib/reader');
+const transform = require('./lib/transform');
+
+// fs.readFile('./assets/bitmap.bmp', (err, data) => {
+//     let bmp = new Bmp(data);
+//     console.log(bmp)
+// });
+
+let type = process.argv[2];
+
+if(type === 'invert') {
+    reader.readAndWrite('./assets/bitmap.bmp', './assets/inverted.bmp', 'invert');
+    reader.readAndWrite('./assets/bitmap.bmp', './__test__/assets/inverted.bmp', 'invert');
+};
+
+if(type === 'flip') {
+    reader.readAndWrite('./assets/bitmap.bmp', './assets/flipped.bmp', 'flip');
+    reader.readAndWrite('./assets/bitmap.bmp', './__test__/assets/flipped.bmp', 'flip');
+};
+
+if(type === 'shuffle') {
+    reader.readAndWrite('./assets/bitmap.bmp', './assets/shuffled.bmp', 'shuffle');
+    reader.readAndWrite('./assets/bitmap.bmp', './__test__/assets/shuffled.bmp', 'shuffle');
+};
+
+if(type === 'mono') {
+    reader.readAndWrite('./assets/bitmap.bmp', './assets/monochromatic.bmp', 'mono');
+    reader.readAndWrite('./assets/bitmap.bmp', './__test__/assets/monochromatic.bmp', 'mono');
+};
